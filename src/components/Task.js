f69136f85d4510e9059ec2dd92382f4f36cb3321
Danny C <76886510+Passiv3@users.noreply.github.com>
2023-03-11 22:44:30 -0800

    Workshop to practice React.js stuff

---
+++ src/components/Task.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Task = () => {
+    return <li></li>;
+};
+
+export default Task;
---
+++ src/components/TaskList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TaskList = () =>{
+    return (
+        <h2>Test</h2>
+        );
+};
+
+export default TaskList;
